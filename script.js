f329f1e309081af30c8c84a447b59be444c97232
aliamk <alia.azarian@gmail.com>
2020-07-28 23:26:12 +0100

    enable adding and deleting items

--- script.js
+++ script.js
@@ -54,10 +54,19 @@ function updateSavedColumns() {
   // localStorage.setItem('onHoldItems', JSON.stringify(onHoldListArray))
 }
 
+// Filter arrays to remove empty items
+function filterArray(array) {
+  // console.log(array)
+  const filteredArray = array.filter(item => item !== null)
+  // console.log(filteredArray)
+  return filteredArray
+}
+
 // Create DOM Elements for each list item
 function createItemEl(columnEl, column, item, index) {
   // console.log('columnEl:', columnEl)
   // console.log('column:', column)
+  // console.log('item:', item)
   // console.log('index:', index)
   // List Item
   const listEl = document.createElement('li')
@@ -65,6 +74,9 @@ function createItemEl(columnEl, column, item, index) {
   listEl.textContent = item
   listEl.draggable = true
   listEl.setAttribute('ondragstart', 'drag(event)')
+  listEl.contentEditable = true
+  listEl.id = index
+  listEl.setAttribute('onfocusout', `updateItem(${index}, ${column})`)
   // Append
   columnEl.appendChild(listEl)
 }
@@ -80,24 +92,45 @@ function updateDOM() {
   backlogListArray.forEach((backlogItem, index) => {
     createItemEl(backlogList, 0, backlogItem, index)
   })
+  backlogListArray = filterArray(backlogListArray)
+
   // Progress Column
   progressList.textContent = '' 
   progressListArray.forEach((progressItem, index) => {
-    createItemEl(progressList, 0, progressItem, index)
+    createItemEl(progressList, 1, progressItem, index)
   })
+  progressListArray = filterArray(progressListArray)
+
   // Complete Column
   completeList.textContent = '' 
   completeListArray.forEach((completeItem, index) => {
-    createItemEl(completeList, 0, completeItem, index)
+    createItemEl(completeList, 2, completeItem, index)
   })
+  completeListArray = filterArray(completeListArray)
+
   // On Hold Column
   onHoldList.textContent = '' 
   onHoldListArray.forEach((onHoldItem, index) => {
-    createItemEl(onHoldList, 0, onHoldItem, index)
+    createItemEl(onHoldList, 3, onHoldItem, index)
   })
+  onHoldListArray = filterArray(onHoldListArray)
   // Run getSavedColumns only once, Update Local Storage
   updatedOnLoad = true
   updateSavedColumns()
+}
+
+// Update Items - Delete if necessay or update the array value
+function updateItem(id, column) {
+  const selectedArray = listArrays[column]
+  // console.log(selectedArray)
+  const selectedColumnEl = listColumns[column].children
+  // console.log(selectedColumnEl[id].textContent)
+  if (!selectedColumnEl[id].textContent) {
+    delete selectedArray[id]
+  }
+  console.log(selectedArray)
+  updateDOM()
+
 }
 
 // Add to column list, reset textbox
